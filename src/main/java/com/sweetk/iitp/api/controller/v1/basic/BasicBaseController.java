fb157e66fa5aae091d04c08dfd5c83df445af9c1
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-04 20:32:03 +0900

    [Change] 기초-주거 자립현황 - 신규등록 장애인현황 관련 API 처리 중

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicBaseController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicBaseController.java
@@ -1,15 +1,17 @@
 package com.sweetk.iitp.api.controller.v1.basic;
 
 import com.sweetk.iitp.api.constant.ApiConstants;
-import com.sweetk.iitp.api.dto.common.ApiResDto;
+import com.sweetk.iitp.api.exception.BusinessException;
 import com.sweetk.iitp.api.exception.ErrorCode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 /**
- *
+ * 기초 공통 API Controller
  */
 @Slf4j
 @RestController
@@ -17,16 +19,47 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "기초 데이터 API V1", description = "기초 데이터 OpenAPI V1 ")
 public class BasicBaseController {
 
+    /*
+
     public static <T> ApiResDto<T> BasicApiOk(T data) {
         return ApiResDto.success(data);
     }
 
     public static <T> ApiResDto<T> BasicApiError(ErrorCode errorCode) {
-        return ApiResDto.error(errorCode);
+        return BusinessException.of(errorCode);
     }
 
     public static <T> ApiResDto<T> BasicApiError(ErrorCode errorCode, String detailMessage) {
-        return ApiResDto.error(errorCode, detailMessage);
+        return ErrApiResDto.of(errorCode, detailMessage);
+    }
+    */
+
+    public void CheckFromYear(String reqURI, Integer fromYear ) {
+        ErrorCode errorCode = isStatFromYearOk(fromYear);
+        if (errorCode != ErrorCode.SUCCESS) {
+            String detailMsg = String.format("from(%s) is invalid (MAx %s Year)", fromYear, ApiConstants.Param.MAX_STAT_YEAR_PERIOD);
+            log.error("[{}] :: {}", reqURI, detailMsg);
+            throw new BusinessException(errorCode, detailMsg);
+        }
     }
 
+    /**
+     * 주어진 fromYear 가 현재년도에서 MAX_STAT_YEAR_PERIOD(10) 이내인지 확인
+     * @param fromYear
+     * @return
+     */
+    public ErrorCode isStatFromYearOk(Integer fromYear) {
+        if (fromYear == null ) {
+            return ErrorCode.INVALID_PARAMETER;
+        }
+
+        Integer currentYear = LocalDate.now().getYear();
+        int diffYear = currentYear - fromYear;
+
+        if(diffYear < 0 || diffYear > ApiConstants.Param.MAX_STAT_YEAR_PERIOD  ) {
+            return ErrorCode.INVALID_INPUT_VALUE;
+        }
+
+        return ErrorCode.SUCCESS;
+    }
 } 
--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
@@ -2,11 +2,22 @@ package com.sweetk.iitp.api.controller.v1.basic;
 
 
 import com.sweetk.iitp.api.constant.ApiConstants;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.common.ApiResDto;
+import com.sweetk.iitp.api.exception.BusinessException;
+import com.sweetk.iitp.api.exception.ErrorCode;
+import com.sweetk.iitp.api.service.basic.BasicHousingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * 기초-주거 자립 현황 API Controller
@@ -15,7 +26,71 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(ApiConstants.ApiPath.API_V1_BASIC_HOUSING)
 @RequiredArgsConstructor
+@Validated
 @Tag(name = "기초-주거 자립 현황 API V1", description = "기초-주거 자립 현황 OpenApi V1")
 public class BasicHousingController extends BasicBaseController {
+    private final BasicHousingService housingService;
 
+
+    /*******************************
+     * 신규등록 장애인현황
+     *******************************/
+
+    @GetMapping("/latest/reg/new")
+    @Operation(
+            summary = "최근 신규등록 장애인현황 조회",
+            description = "최근 신규등록 장애인현황 조회(최대 10년) 예) from(옵션)~(가장 최신연도), default는 가장 최신연도"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegNewLatest(
+            @Parameter(name = "from", description = "(옵션) 검색 시작 연도 (2019 이후 부터 가능, 최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            @Min(value = ApiConstants.Param.START_STAT_YEAR_HOUSING_REG,
+                            message = ApiConstants.Param.START_STAT_YEAR_ERR_MSG)
+            Integer from,
+            HttpServletRequest request) {
+
+        CheckFromYear(request.getRequestURI(), from);
+
+        StatDataRes statDataRes = null;
+        try {
+            statDataRes = housingService.getHousingLatestRegNew(from);
+        }
+        catch (Exception e) {
+            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+    }
+
+    @GetMapping("/latest/reg/new/{statYear}")
+    @Operation(
+            summary = "해당 연도 신규등록 장애인현황 조회",
+            description = "해당 연도의 신규등록 장애인현황 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegNewYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            @Min(value = ApiConstants.Param.START_STAT_YEAR_HOUSING_REG,
+                        message = ApiConstants.Param.START_STAT_YEAR_ERR_MSG)
+            Integer statYear,
+            HttpServletRequest request) {
+
+        StatDataRes statDataRes = null;
+        try {
+            statDataRes = housingService.getHousingLatestRegNew(statYear);
+        }
+        catch (Exception e) {
+            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+    }
+
+
+
+    /*******************************
+     * 최근 신규등록 장애인현황
+     *******************************/
 }
