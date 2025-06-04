9b135c2ce57aaae95d4254cc68aff555f3882c58
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-04 20:28:15 +0900

    [Change] API Req Parameter 관련 정의

---
+++ src/main/java/com/sweetk/iitp/api/constant/SysConstants.java
@@ -0,0 +1,17 @@
+package com.sweetk.iitp.api.constant;
+
+
+public final class SysConstants {
+
+    private SysConstants() {
+        throw new IllegalStateException("Sys Constants class");
+    }
+
+    public static final class Stats {
+        private Stats() {
+            throw new IllegalStateException("Stats Constants class");
+        }
+
+        public static final String KOSIS_META_ITEM_OBJ_NAME = "항목";
+    }
+}
