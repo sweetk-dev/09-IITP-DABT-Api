3514d8b4c8212045e370f3e16c40c0d042dc0612
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-04 20:30:35 +0900

    [Change] Service 처리 구조 변경

--- src/main/java/com/sweetk/iitp/api/service/basic/BasicHealthService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicHealthService.java
@@ -1,16 +1,18 @@
-package com.sweetk.iitp.api.service.basic.impl;
+package com.sweetk.iitp.api.service.basic;
 
 import com.sweetk.iitp.api.repository.basic.health.StatsDisHlthDiseaseCostSubRepository;
 import com.sweetk.iitp.api.repository.basic.health.StatsDisHlthExrcBestAidRepository;
 import com.sweetk.iitp.api.repository.basic.health.StatsDisHlthMedicalUsageRepository;
 import com.sweetk.iitp.api.repository.basic.health.StatsDisHlthSportExecTypeRepository;
-import com.sweetk.iitp.api.service.basic.BasicBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * 기초-건강 관리 현황 API Service
+ */
 @Service
 @RequiredArgsConstructor
-public class BasicHealthServiceImpl extends BasicBaseService {
+public class BasicHealthService extends BasicBaseService {
 
     private final StatsDisHlthDiseaseCostSubRepository diseaseCostSubRepos;
     private final StatsDisHlthExrcBestAidRepository dxrcBestAidRepos;
--- src/main/java/com/sweetk/iitp/api/service/basic/BasicSocialService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicSocialService.java
@@ -1,13 +1,15 @@
-package com.sweetk.iitp.api.service.basic.impl;
+package com.sweetk.iitp.api.service.basic;
 
 import com.sweetk.iitp.api.repository.basic.social.StatsDisSocContactCntfreqRepository;
-import com.sweetk.iitp.api.service.basic.BasicBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * 기초-사회망 현황 API Service
+ */
 @Service
 @RequiredArgsConstructor
-public class BasicSocialServiceImpl extends BasicBaseService {
+public class BasicSocialService extends BasicBaseService {
 
     private final StatsDisSocContactCntfreqRepository contactCntfreqRepos;
 }
