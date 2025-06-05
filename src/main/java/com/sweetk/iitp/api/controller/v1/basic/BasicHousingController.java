e881b3aca2b52830e5e1cd1a4987f391fea7090c
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-05 14:39:24 +0900

    [Change] "/latest/reg/nation" 처리 추가

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
@@ -53,7 +53,7 @@ public class BasicHousingController extends BasicBaseController {
 
         StatDataRes statDataRes = null;
         try {
-            statDataRes = housingService.getHousingLatestRegNew(from);
+            statDataRes = housingService.getHousingRegNewLatest(from);
         }
         catch (Exception e) {
             log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
@@ -91,6 +91,94 @@ public class BasicHousingController extends BasicBaseController {
 
 
     /*******************************
-     * 최근 신규등록 장애인현황
+     * 전국 연령별,장애등급별,성별 등록장애인수
      *******************************/
+    @GetMapping("/latest/reg/nation")
+    @Operation(
+            summary = "최근 신규등록 장애인현황 조회",
+            description = "최근 신규등록 장애인현황 조회(최대 10년) 예) from(옵션)~(가장 최신연도), default는 가장 최신연도"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegNewLatest(
+            @Parameter(name = "from", description = "(옵션) 검색 시작 연도 (2019 이후 부터 가능, 최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            @Min(value = ApiConstants.Param.START_STAT_YEAR_HOUSING_REG,
+                    message = ApiConstants.Param.START_STAT_YEAR_ERR_MSG)
+            Integer from,
+            HttpServletRequest request) {
+
+        CheckFromYear(request.getRequestURI(), from);
+
+        StatDataRes statDataRes = null;
+        try {
+            statDataRes = housingService.getHousingLatestRegNation(from);
+        }
+        catch (Exception e) {
+            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+    }
+
+    @GetMapping("/latest/reg/nat/{statYear}")
+    @Operation(
+            summary = "해당 연도 신규등록 장애인현황 조회",
+            description = "해당 연도의 신규등록 장애인현황 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegNewYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            @Min(value = ApiConstants.Param.START_STAT_YEAR_HOUSING_REG,
+                    message = ApiConstants.Param.START_STAT_YEAR_ERR_MSG)
+            Integer statYear,
+            HttpServletRequest request) {
+
+        StatDataRes statDataRes = null;
+        try {
+            statDataRes = housingService.getHousingLatestRegNew(statYear);
+        }
+        catch (Exception e) {
+            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+    }
+
+
+    /*******************************
+     * 시도별,장애유형별,장애정도별,성별 등록장애인수
+     *******************************/
+
+
+
+    /*******************************
+     * 일상생활 필요 지원 정도
+     *******************************/
+
+
+
+
+    /*******************************
+     * 주로 지원해주는 사람의 유형
+     *******************************/
+
+
+
+    /*******************************
+     * 일상생활 도와주는 사람(1순위)
+     *******************************/
+
+
+
+
+
+    /*******************************
+     * 도움받는 분야
+     *******************************/
+
+
+
+
+
 }
--- src/main/java/com/sweetk/iitp/api/service/basic/BasicHousingService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicHousingService.java
@@ -1,8 +1,14 @@
 package com.sweetk.iitp.api.service.basic;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sweetk.iitp.api.dto.basic.StatDataItem;
 import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.entity.basic.QBaseStatsEntity;
+import com.sweetk.iitp.api.entity.basic.QMetaCodeEntity;
 import com.sweetk.iitp.api.repository.basic.house.*;
+import io.micrometer.core.annotation.Timed;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +21,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BasicHousingService extends BasicBaseService {
 
+    private final EntityManager entityManager;
+    private final JPAQueryFactory queryFactory;
     private final StatsDisLifeMaincarerRepository LifeMaincarerRepos;
     private final StatsDisLifePrimcarerRepository LifePrimcarerRepos;
     private final StatsDisLifeSuppFieldRepository LifeSuppFieldRepos;
@@ -24,13 +32,19 @@ public class BasicHousingService extends BasicBaseService {
     private final StatsDisRegSidoByAgeTypeSevGenRepository RegSidoByAgeTypeSevGenRepos;
 
 
+
+    /*******************************
+     * 신규등록 장애인현황
+     *******************************/
+
     /**
      * 기초-주거 자립 현황-최신 데이터 조회
      *
      * @param fromYear (옵션) 검색 시작 연도 (기본값: 1, 최대: 10년)
      * @return 주거 자립 현황-최신 데이터
      */
-    public StatDataRes getHousingLatestRegNew(Integer fromYear) {
+    @Timed(value = "housing.reg.new.latest", description = "신규등록 장애인현황 조회 시간")
+    public StatDataRes getHousingRegNewLatest(Integer fromYear) {
 
         QBaseStatsEntity stats = QBaseStatsEntity.baseStatsEntity;
         QMetaCodeEntity metaC1 = new QMetaCodeEntity("metaC1");
@@ -60,5 +74,48 @@ public class BasicHousingService extends BasicBaseService {
                 .fetch();
 
     }
+
+
+
+    /*******************************
+     * 전국 연령별,장애등급별,성별 등록장애인수
+     *******************************/
+
+
+
+    /*******************************
+     * 시도별,장애유형별,장애정도별,성별 등록장애인수
+     *******************************/
+
+
+
+    /*******************************
+     * 일상생활 필요 지원 정도
+     *******************************/
+
+
+
+
+    /*******************************
+     * 주로 지원해주는 사람의 유형
+     *******************************/
+
+
+
+    /*******************************
+     * 일상생활 도와주는 사람(1순위)
+     *******************************/
+
+
+
+
+
+    /*******************************
+     * 도움받는 분야
+     *******************************/
+
+
+
+
 
 }
