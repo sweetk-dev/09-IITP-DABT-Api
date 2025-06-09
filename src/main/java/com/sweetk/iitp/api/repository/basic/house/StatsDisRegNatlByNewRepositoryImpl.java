6de6abe8c7bb4cedfd3301177c2d74063766be2c
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-09 20:57:36 +0900

    [Change] Stat data 조회 쿼리 공통 함수 추가

--- src/main/java/com/sweetk/iitp/api/repository/basic/house/StatsDisRegNatlByNewRepositoryImpl.java
+++ src/main/java/com/sweetk/iitp/api/repository/basic/house/StatsDisRegNatlByNewRepositoryImpl.java
@@ -1,9 +1,10 @@
 package com.sweetk.iitp.api.repository.basic.house;
 
-import com.querydsl.core.types.Projections;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
-import com.sweetk.iitp.api.entity.basic.housing.QStatsDisRegNatlByNewEntity;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import com.sweetk.iitp.api.repository.basic.BasicRepositoryQuerySupport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,32 +12,26 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class StatsDisRegNatlByNewRepositoryImpl implements StatsDisRegNatlByNewRepositoryCustom {
+public class StatsDisRegNatlByNewRepositoryImpl extends BasicRepositoryQuerySupport implements StatsDisRegNatlByNewRepositoryCustom {
     
     private final JPAQueryFactory queryFactory;
     
     @Override
-    public List<StatDataItemDB> findLatestRegNewData(Integer fromYear) {
-        QStatsDisRegNatlByNewEntity stats = QStatsDisRegNatlByNewEntity.statsDisRegNatlByNewEntity;
+    public List<StatDataItemDB> findLatestRegNewData(StatsSrcDataInfoEntity srcDataInfo , Integer fromYear) {
+        QBaseStatsEntity stats = QBaseStatsEntity.baseStatsEntity;
 
-        return queryFactory
-                .select(Projections.constructor(StatDataItemDB.class,
-                        stats.prdDe,
-                        stats.c1,
-                        stats.c2,
-                        stats.c3,
-                        stats.c1ObjNm,
-                        stats.c2ObjNm,
-                        stats.c3ObjNm,
-                        stats.itmId,
-                        stats.unitNm,
-                        stats.dt.stringValue(),
-                        stats.lstChnDe,
-                        stats.srcDataId
-                ))
-                .from(stats)
-                .where(stats.prdDe.goe(fromYear))
-                .orderBy(stats.prdDe.asc())
-                .fetch();
+        List<StatDataItemDB> result = buildLatestStatDataItemQuery(stats,srcDataInfo, fromYear );
+
+        return result;
+    }
+
+
+    @Override
+    public List<StatDataItemDB> findTargetRegNewData(StatsSrcDataInfoEntity srcDataInfo , Integer targetYear) {
+        QBaseStatsEntity stats = QBaseStatsEntity.baseStatsEntity;
+
+        List<StatDataItemDB> result = buildTargetStatDataItemQuery(stats, srcDataInfo, targetYear );
+
+        return result;
     }
 } 
