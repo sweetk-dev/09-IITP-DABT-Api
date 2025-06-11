63ea5be84d705f31751b41bbaeca10f83a54e8b2
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-11 18:50:15 +0900

    [Change] DB 조회데이터 캐쉬 설정 수정

--- src/main/java/com/sweetk/iitp/api/config/CacheConfig.java
+++ src/main/java/com/sweetk/iitp/api/config/CacheConfig.java
@@ -1,6 +1,7 @@
 package com.sweetk.iitp.api.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -24,10 +25,11 @@ public class CacheConfig {
             .expireAfterAccess(1, TimeUnit.DAYS)    // 24시간 동안 접근이 없으면 만료
             .maximumSize(1000)                      // 최대 1000개 항목
             .recordStats()                          // 통계 기록
-            .removalListener(notification ->        // 제거 이벤트 리스너
-                log.info("Cache entry removed: {} - {}", 
-                    notification.getKey(), 
-                    notification.getCause()))
+            .removalListener((key, value, cause) -> // 제거 이벤트 리스너
+                log.info("Cache entry removed: {} - {} - {}", 
+                    key, 
+                    value,
+                    cause))
         );
 
         return cacheManager;
