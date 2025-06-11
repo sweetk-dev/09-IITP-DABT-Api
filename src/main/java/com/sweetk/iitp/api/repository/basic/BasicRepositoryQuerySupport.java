0bfc962f830731bf6d694956dc966135fe97bb81
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-11 21:52:08 +0900

    [Add] BaseStatsDtStringEntity 의 buildLatestStatDataItemQuery, buildTargetStatDataItemQuery 추가

--- src/main/java/com/sweetk/iitp/api/repository/basic/BasicRepositoryQuerySupport.java
+++ src/main/java/com/sweetk/iitp/api/repository/basic/BasicRepositoryQuerySupport.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.entity.basic.BaseStatsDtStringEntity;
 import com.sweetk.iitp.api.entity.basic.BaseStatsEntity;
 import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,30 @@ public abstract class BasicRepositoryQuerySupport {
             StatsSrcDataInfoEntity srcDataInfo,
             Integer fromYear) {
 
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        stats.prdDe,
+                        stats.c1,
+                        stats.c2,
+                        stats.c3,
+                        stats.c1ObjNm,
+                        stats.c2ObjNm,
+                        stats.c3ObjNm,
+                        stats.itmId,
+                        stats.unitNm,
+                        stats.dt.stringValue(),
+                        stats.lstChnDe,
+                        stats.srcDataId
+                ))
+                .from(stats)
+                .where(
+                        stats.prdDe.goe(fromYear)
+                                .and(stats.statLatestChnDt.eq(srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(stats.prdDe.asc());
+
+
+        /*
         return queryFactory
                 .select(Projections.constructor(StatDataItemDB.class,
                         Expressions.constant(stats.getMetadata().getName()),
@@ -43,7 +68,10 @@ public abstract class BasicRepositoryQuerySupport {
                                 .and(Expressions.booleanTemplate("src_latest_chn_dt = {0}", srcDataInfo.getStatLatestChnDt()))
                 )
                 .orderBy(Expressions.stringTemplate("prd_de").asc());
+         */
     }
+
+
 
 
     // 특정 년도 통계 테이터 조회 쿼리
@@ -52,6 +80,28 @@ public abstract class BasicRepositoryQuerySupport {
             StatsSrcDataInfoEntity srcDataInfo,
             Integer targetYear) {
 
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        stats.prdDe,
+                        stats.c1,
+                        stats.c2,
+                        stats.c3,
+                        stats.c1ObjNm,
+                        stats.c2ObjNm,
+                        stats.c3ObjNm,
+                        stats.itmId,
+                        stats.unitNm,
+                        stats.dt.stringValue(),
+                        stats.lstChnDe,
+                        stats.srcDataId
+                ))
+                .from(stats)
+                .where(
+                        stats.prdDe.eq(targetYear)
+                                .and(stats.statLatestChnDt.eq(srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(stats.prdDe.asc());
+        /*
         return queryFactory
                 .select(Projections.constructor(StatDataItemDB.class,
                         Expressions.constant(stats.getMetadata().getName()),
@@ -73,5 +123,118 @@ public abstract class BasicRepositoryQuerySupport {
                                 .and(Expressions.booleanTemplate("src_latest_chn_dt = {0}", srcDataInfo.getStatLatestChnDt()))
                 )
                 .orderBy(Expressions.stringTemplate("prd_de").asc());
+
+         */
+    }
+
+
+    // 최근 통계 테이터 조회 쿼리
+    protected <T extends BaseStatsDtStringEntity, Q extends EntityPathBase<T>> JPAQuery<StatDataItemDB> buildLatestStatDataItemQuery(
+            Q stats,
+            StatsSrcDataInfoEntity srcDataInfo,
+            Integer fromYear) {
+
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        stats.prdDe,
+                        stats.c1,
+                        stats.c2,
+                        stats.c3,
+                        stats.c1ObjNm,
+                        stats.c2ObjNm,
+                        stats.c3ObjNm,
+                        stats.itmId,
+                        stats.unitNm,
+                        stats.dt.stringValue(),
+                        stats.lstChnDe,
+                        stats.srcDataId
+                ))
+                .from(stats)
+                .where(
+                        stats.prdDe.goe(fromYear)
+                                .and(stats.statLatestChnDt.eq(srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(stats.prdDe.asc());
+
+
+        /*
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName())
+                ))
+                .from(stats)
+                .where(
+                        Expressions.booleanTemplate("prd_de >= {0}", fromYear)
+                                .and(Expressions.booleanTemplate("src_latest_chn_dt = {0}", srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(Expressions.stringTemplate("prd_de").asc());
+         */
+    }
+
+
+
+
+    // 특정 년도 통계 테이터 조회 쿼리
+    protected <T extends BaseStatsDtStringEntity, Q extends EntityPathBase<T>> JPAQuery<StatDataItemDB> buildTargetStatDataItemQuery(
+            Q stats,
+            StatsSrcDataInfoEntity srcDataInfo,
+            Integer targetYear) {
+
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        stats.prdDe,
+                        stats.c1,
+                        stats.c2,
+                        stats.c3,
+                        stats.c1ObjNm,
+                        stats.c2ObjNm,
+                        stats.c3ObjNm,
+                        stats.itmId,
+                        stats.unitNm,
+                        stats.dt.stringValue(),
+                        stats.lstChnDe,
+                        stats.srcDataId
+                ))
+                .from(stats)
+                .where(
+                        stats.prdDe.eq(targetYear)
+                                .and(stats.statLatestChnDt.eq(srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(stats.prdDe.asc());
+        /*
+        return queryFactory
+                .select(Projections.constructor(StatDataItemDB.class,
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName()),
+                        Expressions.constant(stats.getMetadata().getName())
+                ))
+                .from(stats)
+                .where(
+                        Expressions.booleanTemplate("prd_de = {0}", targetYear)
+                                .and(Expressions.booleanTemplate("src_latest_chn_dt = {0}", srcDataInfo.getStatLatestChnDt()))
+                )
+                .orderBy(Expressions.stringTemplate("prd_de").asc());
+
+         */
     }
 }
