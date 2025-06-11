bb815ac5494bcaccd9ab1dac6ef558d594c3a6a3
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-11 21:55:55 +0900

    [Change] reg/*/info, life/*/info API 추가

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicHousingController.java
@@ -3,6 +3,7 @@ package com.sweetk.iitp.api.controller.v1.basic;
 
 import com.sweetk.iitp.api.constant.ApiConstants;
 import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
 import com.sweetk.iitp.api.dto.common.ApiResDto;
 import com.sweetk.iitp.api.exception.BusinessException;
 import com.sweetk.iitp.api.exception.ErrorCode;
@@ -35,33 +36,49 @@ public class BasicHousingController extends BasicBaseController {
      * 신규등록 장애인현황
      *******************************/
 
-    @GetMapping("/reg/new/latest")
+    @GetMapping("/reg/new/info")
     @Operation(
-            summary = "최근 신규등록 장애인현황 조회",
-            description = "최근 신규등록 장애인현황 조회(최대 10년) 예) from(옵션)~(가장 최신연도), default는 가장 최신연도"
+            summary = "신규등록 장애인현황 통계 정보 조회",
+            description = "신규등록 장애인현황 통계 정보 조회"
     )
-    public ResponseEntity<ApiResDto> getHousingRegNewLatest(
-            @Parameter(name = "from", description = "(옵션) 검색 시작 연도 (2019 이후 부터 가능, 최대: 10년 조회)", example = "2019")
-            @RequestParam(required = false)
-            Integer from,
+    public ResponseEntity<ApiResDto> getHousingRegNewInfo(
             HttpServletRequest request) {
 
-
-        StatDataRes statDataRes = null;
         try {
-            statDataRes = housingService.getHousingRegNewLatest(from);
+            StatInfo statinfo = housingService.getHousingRegNewInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
         }
         catch (Exception e) {
-            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
+    }
 
-        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+
+    @GetMapping("/reg/new/latest")
+    @Operation(
+            summary = "신규등록 장애인현황 조회",
+            description = "신규등록 장애인현황 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegNewLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingRegNewLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/reg/new/{statYear}")
     @Operation(
-            summary = "해당 연도 신규등록 장애인현황 조회",
+            summary = "해당 연도의  신규등록 장애인현황 조회",
             description = "해당 연도의 신규등록 장애인현황 조회"
     )
     public ResponseEntity<ApiResDto> getHousingRegNewYear(
@@ -70,16 +87,16 @@ public class BasicHousingController extends BasicBaseController {
             Integer statYear,
             HttpServletRequest request) {
 
-        StatDataRes statDataRes = null;
         try {
-            statDataRes = housingService.getHousingRegNewYear(statYear);
+            StatDataRes statDataRes = housingService.getHousingRegNewYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
         }
         catch (Exception e) {
-            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+
     }
 
 
@@ -87,33 +104,54 @@ public class BasicHousingController extends BasicBaseController {
     /*******************************
      * 전국 연령별,장애등급별,성별 등록장애인수
      *******************************/
-    @GetMapping("/reg/ageSevGen/latest")
+    @GetMapping("/reg/ageSevGen/info")
     @Operation(
-            summary = "최근 전국 연령별,장애등급별,성별 등록장애인수 조회",
-            description = "최근 전국 연령별,장애등급별,성별 등록장애인수 (최대 10년) 예) from(옵션)~(가장 최신연도), default는 가장 최신연도"
+            summary = "전국 연령별,장애등급별,성별 등록장애인수 통계 정보 조회",
+            description = "전국 연령별,장애등급별,성별 등록장애인수 통계 정보 조회"
     )
-    public ResponseEntity<ApiResDto> getHousingRegAgeSevGenLatest(
-            @Parameter(name = "from", description = "(옵션) 검색 시작 연도 (2019 이후 부터 가능, 최대: 10년 조회)", example = "2019")
-            @RequestParam(required = false)
-            Integer from,
+    public ResponseEntity<ApiResDto> getHousingRegAgeSevGenInfo(
             HttpServletRequest request) {
 
-        StatDataRes statDataRes = null;
         try {
-            statDataRes = housingService.getHousingRegAgeSevGenLatest(from);
+            StatInfo statinfo = housingService.getHousingRegAgeSevGenInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
         }
         catch (Exception e) {
-            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        return ResponseEntity.ok(ApiResDto.success(statDataRes));
+
+    }
+
+
+    @GetMapping("/reg/ageSevGen/latest")
+    @Operation(
+            summary = "전국 연령별,장애등급별,성별 등록장애인수 조회",
+            description = "전국 연령별,장애등급별,성별 등록장애인수 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegAgeSevGenLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingRegAgeSevGenLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 
     @GetMapping("/reg/ageSevGen/{statYear}")
     @Operation(
-            summary = "해당 연도 신규등록 장애인현황 조회",
-            description = "해당 연도의 신규등록 장애인현황 조회"
+            summary = "해당 연도의  전국 연령별,장애등급별,성별 등록장애인수 조회",
+            description = "해당 연도의 전국 연령별,장애등급별,성별 등록장애인수 조회"
     )
     public ResponseEntity<ApiResDto> getHousingRegAgeSevGenYear(
             @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
@@ -121,35 +159,203 @@ public class BasicHousingController extends BasicBaseController {
             Integer statYear,
             HttpServletRequest request) {
 
-        StatDataRes statDataRes = null;
         try {
-            statDataRes = housingService.getHousingRegAgeSevGenYear(statYear);
+            StatDataRes statDataRes = housingService.getHousingRegAgeSevGenYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
         }
         catch (Exception e) {
-            log.error("최근 신규등록 장애인현황 조회 실패, 에러: {}", e.getMessage(), e);
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        return ResponseEntity.ok(ApiResDto.success(statDataRes));
     }
 
 
     /*******************************
      * 시도별,장애유형별,장애정도별,성별 등록장애인수
      *******************************/
+    @GetMapping("/reg/sidoSevGen/latest")
+    @Operation(
+            summary = "시도별,장애등급별,성별 등록장애인수 조회",
+            description = "시도별,장애등급별,성별 등록장애인수 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegSidoASevGenLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
 
+        try {
+            StatDataRes statDataRes = housingService.getHousingRegSidoSevGenLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/reg/sidoSevGen/{statYear}")
+    @Operation(
+            summary = "해당 연도의  시도별,장애등급별,성별 등록장애인수 조회",
+            description = "해당 연도의 시도별,장애등급별,성별 등록장애인수 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingRegSidoSevGenYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingRegSidoSevGenYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
 
     /*******************************
      * 일상생활 필요 지원 정도
      *******************************/
+    @GetMapping("/life/SuppNeedLvl/info")
+    @Operation(
+            summary = "일상생활 필요 지원 정도 통계 정보 조회",
+            description = "일상생활 필요 지원 정도 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppNeedLvlInfo(
+            HttpServletRequest request) {
 
+        try {
+            StatInfo statinfo = housingService.getHousingLifeSuppNeedLvlInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/SuppNeedLvl/latest")
+    @Operation(
+            summary = "일상생활 필요 지원 정도 조회",
+            description = "일상생활 필요 지원 정도 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppNeedLvlLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeSuppNeedLvlLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/SuppNeedLvl/{statYear}")
+    @Operation(
+            summary = "해당 연도의 일상생활 필요 지원 정도 조회",
+            description = "해당 연도의 일상생활 필요 지원 정도 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppNeedLvlYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeSuppNeedLvlYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
 
 
     /*******************************
      * 주로 지원해주는 사람의 유형
      *******************************/
+    @GetMapping("/life/mainCarer/info")
+    @Operation(
+            summary = "주로 지원해주는 사람의 유형 통계 정보 조회",
+            description = "주로 지원해주는 사람의 유형 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeMainCarerInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = housingService.getHousingLifeMaincarerInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/mainCarer/latest")
+    @Operation(
+            summary = "주로 지원해주는 사람의 유형  조회",
+            description = "주로 지원해주는 사람의 유형 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeMainCarerLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeMaincarerLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/mainCarer/{statYear}")
+    @Operation(
+            summary = "해당 연도의 주로 지원해주는 사람의 유형 조회",
+            description = "해당 연도의 주로 지원해주는 사람의 유형 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeMainCarerYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeMaincarerYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
 
 
@@ -157,12 +363,141 @@ public class BasicHousingController extends BasicBaseController {
      * 일상생활 도와주는 사람(1순위)
      *******************************/
 
+    @GetMapping("/life/primCarer/info")
+    @Operation(
+            summary = "일상생활 도와주는 사람(1순위) 통계 정보 조회",
+            description = "일상생활 도와주는 사람(1순위) 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifePrimCarerInfo(
+            HttpServletRequest request) {
 
+        try {
+            StatInfo statinfo = housingService.getHousingLifePrimcarerInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/primCarer/latest")
+    @Operation(
+            summary = "일상생활 도와주는 사람(1순위)  조회",
+            description = "일상생활 도와주는 사람(1순위) 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifePrimCarerLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifePrimcarerLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/life/primCarer/{statYear}")
+    @Operation(
+            summary = "해당 연도의 일상생활 도와주는 사람(1순위) 조회",
+            description = "해당 연도의 일상생활 도와주는 사람(1순위) 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifePrimCarerYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifePrimcarerYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
 
 
     /*******************************
      * 도움받는 분야
      *******************************/
+
+    @GetMapping("/life/suppField/info")
+    @Operation(
+            summary = "도움받는 분야 통계 정보 조회",
+            description = "도움받는 분야 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppFieldInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = housingService.getHousingLifeSuppFieldInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/suppField/latest")
+    @Operation(
+            summary = "도움받는 분야  조회",
+            description = "도움받는 분야 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppFieldLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도 (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeSuppFieldLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @GetMapping("/life/suppField/{statYear}")
+    @Operation(
+            summary = "해당 연도의 도움받는 분야 조회",
+            description = "해당 연도의 도움받는 분야 조회"
+    )
+    public ResponseEntity<ApiResDto> getHousingLifeSuppFieldYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = housingService.getHousingLifeSuppFieldYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
 
 }
