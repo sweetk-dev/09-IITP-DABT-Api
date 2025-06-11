c94d0db09d0c47bcfd6f239e1a833f58813cf9ff
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-11 19:05:22 +0900

    [Add] 사회망 현황 관련 Repository(social) 추가 및 조회 기능 추가

---
+++ src/main/java/com/sweetk/iitp/api/repository/basic/social/SocialRepository.java
@@ -0,0 +1,14 @@
+package com.sweetk.iitp.api.repository.basic.social;
+
+import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SocialRepository {
+    // StatsDisSocContactCntfreq
+    List<StatDataItemDB> findSocContactCntfreqLatest(StatsSrcDataInfoEntity srcDataInfo, Integer fromYear);
+    List<StatDataItemDB> findSocContactCntfreqByYear(StatsSrcDataInfoEntity srcDataInfo, Integer targetYear);
+} 
