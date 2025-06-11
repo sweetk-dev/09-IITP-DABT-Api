e86851a37a5b9320018d6ac22df8d5a599a661cf
DESKTOP-N9GLB2D\HHAN <hhanlee>
2025-06-11 19:18:02 +0900

    [Add] 편의 시설 제공 현황 관련 Repository(Facility) 추가 및 조회 기능 추가

---
+++ src/main/java/com/sweetk/iitp/api/repository/basic/facility/FacilityRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.sweetk.iitp.api.repository.basic.facility;
+
+import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class FacilityRepositoryImpl {
+    private final StatsDisFcltyWelfareUsageRepository fcltyWelfareUsageRepos;
+
+    public List<StatDataItemDB> findFacilitySuppNeedLvlLatest(StatsSrcDataInfoEntity srcDataInfo, Integer fromYear) {
+        return fcltyWelfareUsageRepos.findDataLatest(srcDataInfo, fromYear);
+    }
+
+    public List<StatDataItemDB> findFacilitySuppNeedLvlByYear(StatsSrcDataInfoEntity srcDataInfo, Integer targetYear) {
+        return fcltyWelfareUsageRepos.findDataByYear(srcDataInfo, targetYear);
+    }
+} 
