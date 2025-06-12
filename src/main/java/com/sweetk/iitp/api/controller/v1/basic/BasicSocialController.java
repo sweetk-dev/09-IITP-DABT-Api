319b1740ed939f7d291a59a464d1d8030ca89d27
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-12 15:07:25 +0900

    [Change] 기초-사회망 현황 API 처리 개발중

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicSocialController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicSocialController.java
@@ -2,11 +2,20 @@ package com.sweetk.iitp.api.controller.v1.basic;
 
 
 import com.sweetk.iitp.api.constant.ApiConstants;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.common.ApiResDto;
+import com.sweetk.iitp.api.exception.BusinessException;
+import com.sweetk.iitp.api.exception.ErrorCode;
+import com.sweetk.iitp.api.service.basic.BasicSocialReadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 기초-사회망 현황 API Controller
@@ -17,4 +26,137 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "기초-사회망 현황 API V1", description = "기초-사회망 현황 OpenApi V1")
 public class BasicSocialController extends BasicBaseController {
+    private final BasicSocialReadService socialReadService;
+
+
+
+    /*******************************
+     * 장애인의 사회 참여
+     *******************************/
+    @GetMapping("/social/particFreq/info")
+    @Operation(
+            summary = "장애인의 사회 참여 통계 정보 조회",
+            description = "장애인의 사회 참여 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getSocialParticFreqInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = socialReadService.getSocialParticFreqInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/social/particFreq/latest")
+    @Operation(
+            summary = "장애인의 사회 참여 조회",
+            description = "장애인의 사회 참여 조회. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getSocialParticFreqLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = socialReadService.getSocialParticFreqLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/social/particFreq/{statYear}")
+    @Operation(
+            summary = "해당 연도의 장애인의 사회 참여 조회",
+            description = "해당 연도의 장애인의 사회 참여 조회"
+    )
+    public ResponseEntity<ApiResDto> getSocialParticFreqYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = socialReadService.getSocialParticFreqYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+    /*******************************
+     * 가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도
+     *******************************/
+    @GetMapping("/social/contractFreq/info")
+    @Operation(
+            summary = "가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 통계 정보 조회",
+            description = "가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getSocialContactCntfreqInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = socialReadService.getSocialContactCntfreqInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/social/contractFreq/latest")
+    @Operation(
+            summary = "가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 조회",
+            description = "가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getSocialContactCntfreqLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = socialReadService.getSocialContactCntfreqLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/social/contractFreq/{statYear}")
+    @Operation(
+            summary = "해당 연도의 가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 조회",
+            description = "해당 연도의 가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 조회"
+    )
+    public ResponseEntity<ApiResDto> getSocialContactCntfreqYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = socialReadService.getSocialContactCntfreqYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/sweetk/iitp/api/service/basic/BasicSocialReadService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicSocialReadService.java
@@ -1,9 +1,21 @@
 package com.sweetk.iitp.api.service.basic;
 
-import com.sweetk.iitp.api.repository.basic.social.StatsDisSocContactCntfreqRepository;
+import com.sweetk.iitp.api.annotation.ConditionalTimed;
+import com.sweetk.iitp.api.dto.basic.StatDataItem;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.basic.converter.StatsDataConverter;
+import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.dto.internal.StatMetaCodeDB;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import com.sweetk.iitp.api.repository.basic.social.SocialRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 기초-사회망 현황 API Service
@@ -13,5 +25,122 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BasicSocialReadService extends AbstractBasicService {
 
-    private final StatsDisSocContactCntfreqRepository contactCntfreqRepos;
+    private final SocialRepository socialRepository;
+    private final MetadataService metadataService;
+    private final DataSourceService dataSourceService;
+
+
+
+    /*******************************
+     * 장애인의 사회 참여
+     *******************************/
+
+    public StatInfo getSocialParticFreqInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialParticFreq();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
+
+
+    @ConditionalTimed(value = "basic.Social.ParticFreq.latest", description = "장애인의 사회 참여 데이터 조회")
+    public StatDataRes getSocialParticFreqLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialParticFreq();
+        Integer fromStatYear = getReqFromYear("SocialParticFreqLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = socialRepository.findSocParticFreqELatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getSocialParticFreqYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialParticFreq();
+        Integer statYear = getReqFromYear("SocialParticFreqYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = socialRepository.findSoParticFreqEByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+
+    /*******************************
+     * 가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도
+     *******************************/
+    public StatInfo getSocialContactCntfreqInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialContactCntfreq();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
+
+
+    @ConditionalTimed(value = "basic.Social.ContactCntfreq.latest", description = "가까이 지내는 친구, 이웃, 지인 수 및 만남 빈도 데이터 조회")
+    public StatDataRes getSocialContactCntfreqLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialContactCntfreq();
+        Integer fromStatYear = getReqFromYear("SocialContactCntfreqLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = socialRepository.findSocContactCntfreqLatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getSocialContactCntfreqYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getSocialContactCntfreq();
+        Integer statYear = getReqFromYear("SocialContactCntfreqYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = socialRepository.findSocContactCntfreqByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
 }
