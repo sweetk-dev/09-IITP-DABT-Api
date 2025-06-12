ffbc1d098970bd7a8f2cd4dbabb90461997f003c
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-12 17:56:06 +0900

    [Add] 기초-진로 교육 현황 API 처리 개발

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicEduController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicEduController.java
@@ -2,11 +2,20 @@ package com.sweetk.iitp.api.controller.v1.basic;
 
 
 import com.sweetk.iitp.api.constant.ApiConstants;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.common.ApiResDto;
+import com.sweetk.iitp.api.exception.BusinessException;
+import com.sweetk.iitp.api.exception.ErrorCode;
+import com.sweetk.iitp.api.service.basic.BasicEduReadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 기초-진로 교육 현황 API Controller
@@ -17,4 +26,140 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "기초-진로 교육 현황 API V1", description = "기초-진로 교육 현황 OpenApi V1")
 public class BasicEduController extends BasicBaseController {
+
+    private final BasicEduReadService eduReadService;
+
+
+    /*******************************
+     * 장애인 진로 및 직업교육 실시 여부
+     *******************************/
+    @GetMapping("/vocaExec/info")
+    @Operation(
+            summary = "장애인 진로 및 직업교육 실시 여부 통계 정보 조회",
+            description = "장애인 진로 및 직업교육 실시 여부 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = eduReadService.getEduVocaExecInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/vocaExec/latest")
+    @Operation(
+            summary = "장애인 진로 및 직업교육 실시 여부 조회",
+            description = "장애인 진로 및 직업교육 실시 여부. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = eduReadService.getEduVocaExecLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/vocaExec/{statYear}")
+    @Operation(
+            summary = "해당 연도의 장애인 진로 및 직업교육 실시 여부 조회",
+            description = "해당 연도의 장애인 진로 및 직업교육 실시 여부 조회"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = eduReadService.getEduVocaExecYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+    /*******************************
+     * 장애인 진로 및 직업교육 운영 방법
+     *******************************/
+    @GetMapping("/vocaExecWay/info")
+    @Operation(
+            summary = "장애인 진로 및 직업교육 운영 방법 통계 정보 조회",
+            description = "장애인 진로 및 직업교육 운영 방법 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecWayInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = eduReadService.getEduVocaExecWayInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/vocaExec/latest")
+    @Operation(
+            summary = "장애인 진로 및 직업교육 운영 방법 조회",
+            description = "장애인 진로 및 직업교육 운영 방법. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecWayLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = eduReadService.getEduVocaExecWayLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/vocaExec/{statYear}")
+    @Operation(
+            summary = "해당 연도의 장애인 진로 및 직업교육 운영 방법 조회",
+            description = "해당 연도의 장애인 진로 및 직업교육 운영 방법 조회"
+    )
+    public ResponseEntity<ApiResDto> getEduVocaExecWayYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = eduReadService.getEduVocaExecWayYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
--- src/main/java/com/sweetk/iitp/api/service/basic/BasicEduReadService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicEduReadService.java
@@ -1,11 +1,21 @@
 package com.sweetk.iitp.api.service.basic;
 
+import com.sweetk.iitp.api.annotation.ConditionalTimed;
+import com.sweetk.iitp.api.dto.basic.StatDataItem;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.basic.converter.StatsDataConverter;
+import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.dto.internal.StatMetaCodeDB;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
 import com.sweetk.iitp.api.repository.basic.edu.EduRepository;
-import com.sweetk.iitp.api.repository.basic.edu.StatsDisEduVocaExecRepository;
-import com.sweetk.iitp.api.repository.basic.edu.StatsDisEduVocaExecWayRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 기초-진로 교육 현황 API Service
@@ -22,13 +32,112 @@ public class BasicEduReadService extends AbstractBasicService {
     /*******************************
      * 장애인 진로 및 직업교육 실시 여부
      *******************************/
-    edu_voca_exec
+    public StatInfo getEduVocaExecInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExec();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
+
+
+    @ConditionalTimed(value = "basic.edu.vocaExec.latest", description = "장애인 진로 및 직업교육 실시 여부 데이터 조회")
+    public StatDataRes getEduVocaExecLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExec();
+        Integer fromStatYear = getReqFromYear("EduVocaExecLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = eduRepository.findEduVocaExecLatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getEduVocaExecYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExec();
+        Integer statYear = getReqFromYear("EduVocaExecYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = eduRepository.findEduVocaExecByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
 
 
 
+    /*******************************
+     * 장애인 진로 및 직업교육 운영 방법
+     *******************************/
+    public StatInfo getEduVocaExecWayInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExecWay();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
 
-            /*******************************
-             * 장애인 진로 및 직업교육 운영 방법
-             *******************************/
-            edu_voca_exec_way
+
+    @ConditionalTimed(value = "basic.edu.vocaExecWay.latest", description = "장애인 진로 및 직업교육 운영 방법 데이터 조회")
+    public StatDataRes getEduVocaExecWayLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExecWay();
+        Integer fromStatYear = getReqFromYear("EduVocaExecWayLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = eduRepository.findEduVocaExecWayLatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getEduVocaExecWayYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getEduVocaExecWay();
+        Integer statYear = getReqFromYear("EduVocaExecYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = eduRepository.findEduVocaExecWayByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
 }
