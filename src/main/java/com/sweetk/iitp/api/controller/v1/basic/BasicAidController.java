302bea993d624e54db2ee3cb7249fd645f689ef7
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-12 19:27:22 +0900

    [Add] 기초-보조기기 사용 현황 API 처리 개발

--- src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicAidController.java
+++ src/main/java/com/sweetk/iitp/api/controller/v1/basic/BasicAidController.java
@@ -2,11 +2,20 @@ package com.sweetk.iitp.api.controller.v1.basic;
 
 
 import com.sweetk.iitp.api.constant.ApiConstants;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.common.ApiResDto;
+import com.sweetk.iitp.api.exception.BusinessException;
+import com.sweetk.iitp.api.exception.ErrorCode;
+import com.sweetk.iitp.api.service.basic.BasicAidReadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 기초-보조기기 사용 현황 API Controller
@@ -17,5 +26,138 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "기초-보조기기 사용 현황 API V1", description = "기초-보조기기 사용 현황 OpenApi V1")
 public class BasicAidController extends BasicBaseController {
+
+    private final BasicAidReadService aidReadService;
+
+
+
+    /*******************************
+     * 장애인보조기기 사용여부
+     *******************************/
+    @GetMapping("/deviceUsg/info")
+    @Operation(
+            summary = "장애인보조기기 사용여부 통계 정보 조회",
+            description = "장애인보조기기 사용여부 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceUsageInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = aidReadService.getAidDeviceUsageInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/deviceUsg/latest")
+    @Operation(
+            summary = "장애인보조기기 사용여부 조회",
+            description = "장애인보조기기 사용여부. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceUsageLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = aidReadService.getAidDeviceUsageLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/deviceUsg/{statYear}")
+    @Operation(
+            summary = "해당 연도의 장애인보조기기 사용여부 조회",
+            description = "해당 연도의 장애인보조기기 사용여부 조회"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceUsageYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = aidReadService.getAidDeviceUsageYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /*******************************
+     * 장애인보조기기 필요여부
+     *******************************/
+    @GetMapping("/deviceNeed/info")
+    @Operation(
+            summary = "장애인보조기기 필요여부 통계 정보 조회",
+            description = "장애인보조기기 필요여부 통계 정보 조회"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceNeedInfo(
+            HttpServletRequest request) {
+
+        try {
+            StatInfo statinfo = aidReadService.getAidDeviceNeedInfo();
+            return ResponseEntity.ok(ApiResDto.success(statinfo));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/deviceNeed/latest")
+    @Operation(
+            summary = "장애인보조기기 필요여부 조회",
+            description = "장애인보조기기 필요여부. (최대 10년) 예) fromYear(옵션)~(최종 연도), default는 최종 3개년"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceNeedLatest(
+            @Parameter(name = "fromYear", description = "(옵션) 검색 시작 연도  (최대: 10년 조회)", example = "2019")
+            @RequestParam(required = false)
+            Integer fromYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = aidReadService.getAidDeviceNeedLatest(fromYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/deviceNeed/{statYear}")
+    @Operation(
+            summary = "해당 연도의 장애인보조기기 필요여부 조회",
+            description = "해당 연도의 장애인보조기기 필요여부 조회"
+    )
+    public ResponseEntity<ApiResDto> getAidDeviceNeedYear(
+            @Parameter(name = "statYear", description = "통계 연도", required = true, example = "2024")
+            @PathVariable
+            Integer statYear,
+            HttpServletRequest request) {
+
+        try {
+            StatDataRes statDataRes = aidReadService.getAidDeviceNeedYear(statYear);
+            return ResponseEntity.ok(ApiResDto.success(statDataRes));
+        }
+        catch (Exception e) {
+            log.error("{} Fail,Error: {}", request.getRequestURI(), e.getMessage(), e);
+            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
--- src/main/java/com/sweetk/iitp/api/service/basic/BasicAidReadService.java
+++ src/main/java/com/sweetk/iitp/api/service/basic/BasicAidReadService.java
@@ -1,10 +1,21 @@
 package com.sweetk.iitp.api.service.basic;
 
-import com.sweetk.iitp.api.repository.basic.aid.StatsDisAidDeviceNeedRepository;
-import com.sweetk.iitp.api.repository.basic.aid.StatsDisAidDeviceUsageRepository;
+import com.sweetk.iitp.api.annotation.ConditionalTimed;
+import com.sweetk.iitp.api.dto.basic.StatDataItem;
+import com.sweetk.iitp.api.dto.basic.StatDataRes;
+import com.sweetk.iitp.api.dto.basic.StatInfo;
+import com.sweetk.iitp.api.dto.basic.converter.StatsDataConverter;
+import com.sweetk.iitp.api.dto.internal.StatDataItemDB;
+import com.sweetk.iitp.api.dto.internal.StatMetaCodeDB;
+import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import com.sweetk.iitp.api.repository.basic.aid.AidRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 기초-보조기기 사용 현황 API Service
@@ -15,6 +26,119 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BasicAidReadService extends AbstractBasicService {
 
-    private final StatsDisAidDeviceNeedRepository deviceNeedRepos;
-    private final StatsDisAidDeviceUsageRepository deviceUsageRepos;
+    private final AidRepository aidRepository;
+    private final MetadataService metadataService;
+    private final DataSourceService dataSourceService;
+
+
+    /*******************************
+     * 장애인보조기기 사용여부
+     *******************************/
+    public StatInfo getAidDeviceUsageInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceUsage();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
+
+    @ConditionalTimed(value = "basic.aid.deviceUsg.latest", description = "장애인보조기기 사용여부 데이터 조회")
+    public StatDataRes getAidDeviceUsageLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceUsage();
+        Integer fromStatYear = getReqFromYear("AidDeviceUsageLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = aidRepository.findAidDeviceUsageLatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getAidDeviceUsageYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceUsage();
+        Integer statYear = getReqFromYear("AidDeviceUsageYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = aidRepository.findAidDeviceUsageByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+
+    /*******************************
+     * 장애인보조기기 필요여부
+     *******************************/
+    public StatInfo getAidDeviceNeedInfo() {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceNeed();
+        return StatsDataConverter.toStatInfoResponse(srcDataInfo);
+    }
+
+    @ConditionalTimed(value = "basic.aid.deviceNeed.latest", description = "장애인보조기기 필요여부 데이터 조회")
+    public StatDataRes getAidDeviceNeedLatest(Integer fromYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceNeed();
+        Integer fromStatYear = getReqFromYear("AidDeviceNeedLatest", fromYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = aidRepository.findAidDeviceNeedLatest(srcDataInfo, fromStatYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+    public StatDataRes getAidDeviceNeedYear(Integer targetYear) {
+        // 1. 데이터 소스 정보 조회
+        StatsSrcDataInfoEntity srcDataInfo = dataSourceService.getAidDeviceNeed();
+        Integer statYear = getReqFromYear("AidDeviceNeedYear", targetYear,
+                srcDataInfo.toIntCollectStartDt(), srcDataInfo.toIntCollectEndDt());
+
+        // 2. 기본 데이터 조회
+        List<StatDataItemDB> dataList = aidRepository.findAidDeviceNeedByYear(srcDataInfo, statYear);
+        if (dataList.isEmpty()) {
+            return StatsDataConverter.toResponseFromItems(srcDataInfo, Collections.emptyList());
+        }
+
+        // 3. 메타 코드 정보 조회
+        LocalDate srcLatestChnDate = LocalDate.parse(srcDataInfo.getStatLatestChnDt());
+        List<StatMetaCodeDB> cMetaCodes = metadataService.getCatMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+        List<StatMetaCodeDB> iMetaCodes = metadataService.getItemMetaCodes(srcDataInfo.getSrcDataId(), srcLatestChnDate);
+
+        // 4. 데이터 변환 및 응답 생성
+        List<StatDataItem> items = makeStatDataItemList(dataList, cMetaCodes, iMetaCodes);
+        return StatsDataConverter.toResponseFromItems(srcDataInfo, items);
+    }
+
+
 }
