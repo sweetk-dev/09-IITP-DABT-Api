b947904f5f58f96297c8cd50c9f16e4abb51ef28
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-13 19:40:12 +0900

    [Add] POI Search 처리 DTO 추가

---
+++ src/main/java/com/sweetk/iitp/api/dto/poi/MvPoiSearchCatReq.java
@@ -0,0 +1,21 @@
+package com.sweetk.iitp.api.dto.poi;
+
+import com.sweetk.iitp.api.constant.MvPoiCategoryType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(description = "POI 검색 요청")
+public class MvPoiSearchCatReq {
+//    @Schema(description = "언어권 (선택사항)", example = "ko")
+//    private String lang = "ko";
+
+    @Schema(description = "카테고리 타입(옵션)", example = "restaurant")
+    private MvPoiCategoryType catType;
+
+    @Schema(description = "catType의 서브 카테고리 타입(옵션)", example = "한식, 카페")
+    private MvPoiCategoryType subCateType;
+
+    @Schema(description = "검색어 (옵션) (단독 세팅으로 검색 가능)", example = "서대문형무소역사관")
+    private String name;
+} 
