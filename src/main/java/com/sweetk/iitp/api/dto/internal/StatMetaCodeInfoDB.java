d5a5f902619ffba3f3a838cbc01ccc7bb02d6fcf
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-16 19:52:15 +0900

    [Change]  DTO 변경

--- src/main/java/com/sweetk/iitp/api/dto/internal/StatMetaCodeInfoDB.java
+++ src/main/java/com/sweetk/iitp/api/dto/internal/StatMetaCodeInfoDB.java
@@ -1,5 +1,14 @@
 package com.sweetk.iitp.api.dto.internal;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class StatMetaCodeInfoDB {
     String itemId;
     String itemNm;
--- src/main/java/com/sweetk/iitp/api/dto/poi/MvPoiSearchLocReq.java
+++ src/main/java/com/sweetk/iitp/api/dto/poi/MvPoiSearchLocReq.java
@@ -3,9 +3,13 @@ package com.sweetk.iitp.api.dto.poi;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 
-@Data
+import java.math.BigDecimal;
+
+@Getter
+@ToString
 @Schema(description = "POI 검색 요청")
 public class MvPoiSearchLocReq {
 //    @Schema(description = "언어권 (선택사항)", example = "ko")
@@ -14,14 +18,14 @@ public class MvPoiSearchLocReq {
     @Schema(description = "위도 (searchType이 location일 때만 사용)", example = "37.5665")
     @Min(value = -90, message = "Latitude must be between -90 and 90")
     @Max(value = 90, message = "Latitude must be between -90 and 90")
-    private Double latitude;
+    private BigDecimal latitude;
 
     @Schema(description = "경도 (searchType이 location일 때만 사용)", example = "126.9780")
     @Min(value = -180, message = "Longitude must be between -180 and 180")
     @Max(value = 180, message = "Longitude must be between -180 and 180")
-    private Double longitude;
+    private BigDecimal longitude;
 
     @Schema(description = "검색 반경(미터) (searchType이 location일 때만 사용)", example = "1000")
     @Min(value = 0, message = "Radius must be positive")
-    private Double radius;
+    private BigDecimal radius;
 } 
