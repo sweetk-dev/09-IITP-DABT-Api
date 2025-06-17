439d81a4c00e9b58d7119d8aa5872e6d49bfa671
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-17 20:36:30 +0900

    [Change] ApiClientEntity 컬럼 정리

--- src/main/java/com/sweetk/iitp/api/entity/ApiClientEntity.java
+++ src/main/java/com/sweetk/iitp/api/entity/ApiClientEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "api_clients")
 @Getter
@@ -16,21 +18,42 @@ public class ApiClientEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
+    private boolean active = false;
+
+    @Column(name = "client_id", length = 40, unique = true, nullable = false)
     private String clientId;
 
-    @Column(nullable = false)
+    @Column(name = "api_key", length = 60, unique = true, nullable = false)
     private String apiKey;
 
-    @Column(nullable = false)
+    @Column(name = "client_name", length = 160, nullable = false)
     private String clientName;
 
-    @Column(nullable = false)
+    @Column(length = 600)
     private String description;
 
-    @Column(nullable = false)
-    private boolean active = true;
 
-    @Column(nullable = false)
-    private String role;
+    @Column(length = 600)
+    private String note;
+
+
+//    @Column(name = "api_usage_start_time")
+//    @Comment("API 사용 시작 시간")
+//    private LocalDateTime apiUsageStartTime;
+//
+//    @Column(name = "api_usage_end_time")
+//    private LocalDateTime apiUsageEndTime;
+
+    @Column(name = "approval_at")
+    private LocalDateTime approvalAt;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
