ad3e24bca2e51267b97b14020cc6479fefad5389
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-17 13:45:28 +0900

    [Change]  PageReq NotNull 추가

--- src/main/java/com/sweetk/iitp/api/dto/common/PageReq.java
+++ src/main/java/com/sweetk/iitp/api/dto/common/PageReq.java
@@ -2,18 +2,21 @@ package com.sweetk.iitp.api.dto.common;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
 @Data
 @Schema(description = "페이징 요청 포맷")
 public class PageReq {
+    @NotNull
     @Min(value = 0, message = "Page number must be greater than or equal to 0")
-    @Schema(description = "페이지 번호, 0 이상.")
+    @Schema(description = "페이지 번호, 0 이상.", required = true)
     private int page = 0;
 
+    @NotNull
     @Min(value = 10, message = "Page size must be greater than or equal to 10")
-    @Schema(description = "페이지 사이즈 , 0 이상.")
+    @Schema(description = "페이지 사이즈 , 0 이상.", required = true)
     private int size;
 
 //    @Schema(description = "정렬 기준 (id, name)등 ", example = "id")
