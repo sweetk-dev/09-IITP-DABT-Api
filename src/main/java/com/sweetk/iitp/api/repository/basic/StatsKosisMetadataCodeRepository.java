653754703893d6808924085feff180ff09cb7e51
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-19 21:08:34 +0900

    [Change] Repository Custom class 합침

--- src/main/java/com/sweetk/iitp/api/repository/basic/StatsKosisMetadataCodeRepository.java
+++ src/main/java/com/sweetk/iitp/api/repository/basic/StatsKosisMetadataCodeRepository.java
@@ -1,8 +1,21 @@
 package com.sweetk.iitp.api.repository.basic;
 
+import com.sweetk.iitp.api.dto.internal.StatMetaCodeDB;
 import com.sweetk.iitp.api.entity.basic.StatsKosisMetadataCodeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+
+interface StatsKosisMetadataCodeRepositoryCustom {
+    List<StatMetaCodeDB> findItemInfoByCatObj(Integer srcDataId, LocalDate statLatestChnDt);
+    List<StatMetaCodeDB> findItemInfoByItemObj(Integer srcDataId, LocalDate statLatestChnDt);
+    List<StatMetaCodeDB> findMetaCodesByIds(Integer srcDataId, LocalDate statLatestChnDt, Set<String> itemIds);
+}
+
 
 @Repository
 public interface StatsKosisMetadataCodeRepository extends JpaRepository<StatsKosisMetadataCodeEntity, Integer>,
