631012de15179465464a49097cb242387bbbd678
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-20 19:04:35 +0900

    [Change] StatsSrcDataInfoEntity 조회시 comman code 매핑 방식 변경

--- src/main/java/com/sweetk/iitp/api/repository/basic/StatsSrcDataInfoRepositoryImpl.java
+++ src/main/java/com/sweetk/iitp/api/repository/basic/StatsSrcDataInfoRepositoryImpl.java
@@ -1,29 +1,46 @@
 package com.sweetk.iitp.api.repository.basic;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sweetk.iitp.api.entity.basic.QStatsSrcDataInfoEntity;
 import com.sweetk.iitp.api.entity.basic.StatsSrcDataInfoEntity;
+import com.sweetk.iitp.api.entity.sys.QSysCommonCodeEntity;
 import com.sweetk.iitp.api.entity.sys.SysCommonCodeEntity;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class StatsSrcDataInfoRepositoryImpl implements StatsSrcDataInfoRepositoryCustom {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    private final QStatsSrcDataInfoEntity qSrc = QStatsSrcDataInfoEntity.statsSrcDataInfoEntity;
+    private final QSysCommonCodeEntity qCode = QSysCommonCodeEntity.sysCommonCodeEntity;
 
     @Override
     public StatsSrcDataInfoEntity findWithStatOrgByIntgTblId(String intgTblId) {
-        StatsSrcDataInfoEntity entity = em.find(StatsSrcDataInfoEntity.class, intgTblId);
-        if (entity != null) {
-            SysCommonCodeEntity statOrg = em.createQuery(
-                "SELECT c FROM SysCommonCodeEntity c WHERE c.codeId = :codeId AND c.grpId = :grpId", SysCommonCodeEntity.class)
-                .setParameter("codeId", entity.getStatOrgId())
-                .setParameter("grpId", "stats_src_orgId")
-                .getResultStream()
-                .findFirst()
-                .orElse(null);
-            entity.setStatOrg(statOrg);
+
+        Tuple tuple = queryFactory
+            .select(qSrc, qCode)
+            .from(qSrc)
+            .leftJoin(qCode)
+                .on(qSrc.statOrgId.eq(qCode.codeId)
+                    .and(qCode.grpId.eq("stats_src_orgId")))
+            .where(qSrc.intgTblId.eq(intgTblId))
+            .fetchFirst();
+
+        if (tuple == null) {
+            return null;
+        }
+
+        StatsSrcDataInfoEntity entity = tuple.get(qSrc);
+        SysCommonCodeEntity statOrg = tuple.get(qCode);
+        entity.setStatOrg(statOrg);
+        if (statOrg != null) {
+            entity.setStatOrgName(statOrg.getCodeNm());
         }
         return entity;
     }
@@ -31,17 +48,26 @@ public class StatsSrcDataInfoRepositoryImpl implements StatsSrcDataInfoRepositor
 
     @Override
     public StatsSrcDataInfoEntity findWithStatOrgNameByIntgTblId(String intgTblId) {
-        StatsSrcDataInfoEntity entity = em.find(StatsSrcDataInfoEntity.class, intgTblId);
-        if (entity != null) {
-            String statOrgName = em.createQuery(
-                            "SELECT c.codeNm FROM SysCommonCodeEntity c WHERE c.codeId = :codeId AND c.grpId = :grpId", String.class)
-                    .setParameter("codeId", entity.getStatOrgId())
-                    .setParameter("grpId", "stats_src_orgId")
-                    .getResultStream()
-                    .findFirst()
-                    .orElse("");
-            entity.setStatOrgName(statOrgName);
+
+
+        // 조인하여 기관명(codeNm)까지 한 번에 조회
+        Tuple tuple = queryFactory
+                .select(qSrc, qCode.codeNm)
+                .from(qSrc)
+                .leftJoin(qCode)
+                .on(qSrc.statOrgId.eq(qCode.codeId)
+                        .and(qCode.grpId.eq("stats_src_orgId")))
+                .where(qSrc.intgTblId.eq(intgTblId))
+                .fetchFirst();
+
+        if (tuple == null) {
+            return null;
         }
+
+        StatsSrcDataInfoEntity entity = tuple.get(qSrc);
+        String statOrgName = tuple.get(qCode.codeNm);
+        entity.setStatOrgName(statOrgName);
+
         return entity;
     }
 } 
