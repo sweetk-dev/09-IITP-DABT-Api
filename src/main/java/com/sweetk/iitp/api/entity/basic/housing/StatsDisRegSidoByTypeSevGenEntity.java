38c06b2ce0ac510167703f0398b3b1bde4a7608f
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-06-23 16:10:16 +0900

    [Change] stats_dis_reg_sido_by_age_type_sev_gen 이름 변경 stats_dis_reg_sido_by_type_sev_gen 로

--- src/main/java/com/sweetk/iitp/api/entity/basic/housing/StatsDisRegSidoByTypeSevGenEntity.java
+++ src/main/java/com/sweetk/iitp/api/entity/basic/housing/StatsDisRegSidoByTypeSevGenEntity.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
  * 기초-주거 자립 현황-시도별,장애유형별,장애정도별,성별 등록장애인수
  */
 @Entity
-@Table(name = "stats_dis_reg_sido_by_age_type_sev_gen")
+@Table(name = "stats_dis_reg_sido_by_type_sev_gen")
 @NoArgsConstructor
 public class StatsDisRegSidoByTypeSevGenEntity extends BaseStatsEntity {
 }
