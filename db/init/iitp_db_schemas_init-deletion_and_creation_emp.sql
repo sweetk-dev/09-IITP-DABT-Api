3e2ceec4e5d7f87c30af9aba8351213722b42a1c
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-04 17:16:08 +0900

    [Add]  고용관련 정적 데이터용 테이블 생성 스크립트 작업 시작

---
+++ db/init/iitp_db_schemas_init-deletion_and_creation_emp.sql
@@ -0,0 +1,7 @@
+-- ## iitp DB Schemas - Initial setup - Creation and Delete if tables exists 
+-- ## ver 0.0.1 last update data : 2025.07.04
+-- ## Only for PostgreSQL
+-- ## 고용관련 정적 데이터용 테이블 생성 스크립트 
+-- ## 나라 HR 데이터 제공 데이터
+
+
