aab81597fd1f03b40684e1ea63b3195690a2934e
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-08 20:59:32 +0900

    [Add] EmpConstants 추가

---
+++ src/main/java/com/sweetk/iitp/api/constant/EmpConstants.java
@@ -0,0 +1,17 @@
+package com.sweetk.iitp.api.constant;
+
+public final class EmpConstants {
+    private EmpConstants() {
+        throw new IllegalStateException("EmpConstants class");
+    }
+
+    public static final class Api {
+        private Api() {
+            throw new IllegalStateException("EmpConstants - Api class");
+        }
+
+        public static final String APPLY_DATE_NO = "별도모집안내";
+    }
+
+
+}
