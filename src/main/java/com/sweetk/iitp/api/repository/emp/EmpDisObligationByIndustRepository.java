4113f1b837b0255b59783d90d8cf12178976e8b1
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-10 19:56:34 +0900

    [Add] emp dis job 관련 통합 으로 개별 Controller, Serivce, Repository 삭제

--- src/main/java/com/sweetk/iitp/api/repository/emp/EmpDisObligationByIndustRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sweetk.iitp.api.repository.emp;
-
-import com.sweetk.iitp.api.entity.emp.EmpDisIndustryObligationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpDisObligationByIndustRepository extends JpaRepository<EmpDisIndustryObligationEntity, Integer> {
-} 
