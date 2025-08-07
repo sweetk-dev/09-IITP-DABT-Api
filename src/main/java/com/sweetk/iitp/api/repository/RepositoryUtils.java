ad5e0f0e3023f1ebe87f34276890fc0414892bfd
hhanleeSwk <hhanlee@sweetk.co.kr>
2025-08-07 19:18:09 +0900

    [Add]  SQL문 처리 유틸 추가

---
+++ src/main/java/com/sweetk/iitp/api/repository/RepositoryUtils.java
@@ -0,0 +1,14 @@
+package com.sweetk.iitp.api.repository;
+
+public class RepositoryUtils {
+    public static String escapeSql(String input) {
+        if (input == null) return null;
+        return input.replace("'", "''");
+    }
+
+    public static StringBuilder addQueryOffset(StringBuilder baseQuery, int offset, int size) {
+        if (baseQuery == null) return null;
+        return baseQuery.append(" OFFSET ").append(offset).append(" LIMIT ").append(size);
+    }
+
+}
